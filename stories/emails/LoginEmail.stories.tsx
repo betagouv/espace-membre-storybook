87418dcc40389733d9ddb803b5ceae3dd5438120
Lucas Charrier <lucharrier@gmail.com>
2025-03-11 16:10:30 +0100

    chore: removed fullname

---
+++ stories/emails/LoginEmail.stories.tsx
@@ -0,0 +1,63 @@
+import {
+    LoginEmail,
+    LoginEmailTitle,
+} from "@/server/views/templates/emails/LoginEmail/LoginEmail";
+import { StoryHelperFactory } from "@docs/.storybook/helpers";
+import { playFindEmailStructure } from "@docs/.storybook/testing";
+import {
+    commonEmailsParameters,
+    withEmailClientOverviewFactory,
+    withEmailRenderer,
+} from "@em/docs/.storybook/email";
+import { Domaine, EmailStatusCode } from "@em/src/models/member";
+import { Meta, StoryFn } from "@storybook/react";
+import { addMonths } from "date-fns/addMonths";
+import { subMonths } from "date-fns/subMonths";
+
+type ComponentType = typeof LoginEmail;
+const { generateMetaDefault, prepareStory } =
+    StoryHelperFactory<ComponentType>();
+
+export default {
+    title: "Emails/Templates/LoginEmail",
+    component: LoginEmail,
+    ...generateMetaDefault({
+        parameters: {
+            ...commonEmailsParameters,
+            docs: {
+                description: {
+                    component: "Email de connexion à l'espace-membre",
+                },
+            },
+        },
+    }),
+} as Meta<ComponentType>;
+
+const Template: StoryFn<ComponentType> = (args) => {
+    return <LoginEmail {...args} />;
+};
+
+const NormalStory = Template.bind({});
+NormalStory.args = {
+    loginUrlWithToken: "https://espace-membre.incubateur.net/dummy-token",
+};
+NormalStory.decorators = [withEmailRenderer];
+NormalStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const Normal = prepareStory(NormalStory);
+
+const ClientOverviewStory = Template.bind({});
+ClientOverviewStory.args = {
+    ...NormalStory.args,
+};
+ClientOverviewStory.decorators = [
+    withEmailRenderer,
+    withEmailClientOverviewFactory(LoginEmailTitle()),
+];
+ClientOverviewStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const ClientOverview = prepareStory(ClientOverviewStory);
