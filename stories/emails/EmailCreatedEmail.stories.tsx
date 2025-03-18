3e085e800e42d83295ae20972284820f25ca4d9e
Lucas Charrier <lucharrier@gmail.com>
2025-03-18 15:34:51 +0100

    feat: make email created email an mjml template

---
+++ stories/emails/EmailCreatedEmail.stories.tsx
@@ -0,0 +1,63 @@
+import { StoryHelperFactory } from "@docs/.storybook/helpers";
+import { playFindEmailStructure } from "@docs/.storybook/testing";
+import {
+    commonEmailsParameters,
+    withEmailClientOverviewFactory,
+    withEmailRenderer,
+} from "@em/docs/.storybook/email";
+import { Domaine, EmailStatusCode } from "@em/src/models/member";
+import {
+    EmailCreatedEmail,
+    EmailCreatedEmailTitle,
+} from "@em/src/server/views/templates/emails/EmailCreatedEmail/EmailCreatedEmail";
+import { Meta, StoryFn } from "@storybook/react";
+type ComponentType = typeof EmailCreatedEmail;
+const { generateMetaDefault, prepareStory } =
+    StoryHelperFactory<ComponentType>();
+
+export default {
+    title: "Emails/Templates/EmailCreatedEmail",
+    component: EmailCreatedEmail,
+    ...generateMetaDefault({
+        parameters: {
+            ...commonEmailsParameters,
+            docs: {
+                description: {
+                    component:
+                        "Email envoyé au membre pour lui dire que son email beta.gouv.fr a été créé",
+                },
+            },
+        },
+    }),
+} as Meta<ComponentType>;
+
+const Template: StoryFn<ComponentType> = (args) => {
+    return <EmailCreatedEmail {...args} />;
+};
+
+const NormalStory = Template.bind({});
+NormalStory.args = {
+    email: "madeleine.durand@beta.gouv.fr",
+    secondaryEmail: "madeleine.durant@gmail.com",
+    secretariatUrl: "https://espace-membre.com",
+};
+NormalStory.decorators = [withEmailRenderer];
+NormalStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const Normal = prepareStory(NormalStory);
+
+const ClientOverviewStory = Template.bind({});
+ClientOverviewStory.args = {
+    ...NormalStory.args,
+};
+ClientOverviewStory.decorators = [
+    withEmailRenderer,
+    withEmailClientOverviewFactory(EmailCreatedEmailTitle()),
+];
+ClientOverviewStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const ClientOverview = prepareStory(ClientOverviewStory);
