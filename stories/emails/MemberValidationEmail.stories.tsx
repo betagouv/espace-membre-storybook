970c5ab5c02ce8e862c8c2296803edf8c8705042
Lucas Charrier <lucharrier@gmail.com>
2025-03-18 14:24:37 +0100

    chore: add all email stories

---
+++ stories/emails/MemberValidationEmail.stories.tsx
@@ -0,0 +1,93 @@
+import { StoryHelperFactory } from "@docs/.storybook/helpers";
+import { playFindEmailStructure } from "@docs/.storybook/testing";
+import {
+    commonEmailsParameters,
+    withEmailClientOverviewFactory,
+    withEmailRenderer,
+} from "@em/docs/.storybook/email";
+import { Domaine, EmailStatusCode } from "@em/src/models/member";
+import {
+    MemberValidationEmail,
+    MemberValidationEmailTitle,
+} from "@em/src/server/views/templates/emails/memberValidationEmail/memberValidationEmail";
+import { Meta, StoryFn } from "@storybook/react";
+import { addMonths } from "date-fns/addMonths";
+import { subMonths } from "date-fns/subMonths";
+type ComponentType = typeof MemberValidationEmail;
+const { generateMetaDefault, prepareStory } =
+    StoryHelperFactory<ComponentType>();
+
+export default {
+    title: "Emails/Templates/MemberValidationEmail",
+    component: MemberValidationEmail,
+    ...generateMetaDefault({
+        parameters: {
+            ...commonEmailsParameters,
+            docs: {
+                description: {
+                    component:
+                        "Email envoyé aux membres transverse de l'incubateur pour les informer qu'il y a une fiche à valider",
+                },
+            },
+        },
+    }),
+} as Meta<ComponentType>;
+
+const Template: StoryFn<ComponentType> = (args) => {
+    return <MemberValidationEmail {...args} />;
+};
+
+const NormalStory = Template.bind({});
+NormalStory.args = {
+    userInfos: {
+        uuid: "",
+        username: "lucas.thenet",
+        fullname: "Lucas Thenet",
+        role: "Développeur Fullstack    ",
+        missions: [
+            {
+                start: new Date(),
+            },
+        ],
+        domaine: Domaine.ANIMATION,
+        primary_email_status: EmailStatusCode.EMAIL_ACTIVE,
+    },
+    validationLink:
+        "https://espace-membre.incubateur.net/community/lucas.thenet/validate",
+    startups: [
+        {
+            uuid: "",
+            ghid: "jagis",
+            name: "J'agis",
+            incubator_id: "",
+            start: new Date(),
+        },
+    ],
+    incubator: {
+        uuid: "",
+        title: "Incubateur de service numérique de la Dinum",
+        ghid: "",
+        description: "",
+        short_description: "",
+    },
+};
+NormalStory.decorators = [withEmailRenderer];
+NormalStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const Normal = prepareStory(NormalStory);
+
+const ClientOverviewStory = Template.bind({});
+ClientOverviewStory.args = {
+    ...NormalStory.args,
+};
+ClientOverviewStory.decorators = [
+    withEmailRenderer,
+    withEmailClientOverviewFactory(MemberValidationEmailTitle()),
+];
+ClientOverviewStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const ClientOverview = prepareStory(ClientOverviewStory);
---
+++ stories/emails/StartupNewMemberArrivalEmail.stories.ignore.tsx
@@ -0,0 +1,82 @@
+import { StoryHelperFactory } from "@docs/.storybook/helpers";
+import { playFindEmailStructure } from "@docs/.storybook/testing";
+import {
+    commonEmailsParameters,
+    withEmailClientOverviewFactory,
+    withEmailRenderer,
+} from "@em/docs/.storybook/email";
+import { Domaine, EmailStatusCode } from "@em/src/models/member";
+import {
+    StartupNewMemberArrivalEmail,
+    StartupNewMemberArrivalEmailTitle,
+} from "@em/src/server/views/templates/emails/StartupNewMemberArrivalEmail/StartupNewMemberArrivalEmail";
+import { Meta, StoryFn } from "@storybook/react";
+import { addMonths } from "date-fns/addMonths";
+import { subMonths } from "date-fns/subMonths";
+type ComponentType = typeof StartupNewMemberArrivalEmail;
+const { generateMetaDefault, prepareStory } =
+    StoryHelperFactory<ComponentType>();
+
+export default {
+    title: "Emails/Templates/StartupNewMemberArrivalEmail",
+    component: StartupNewMemberArrivalEmail,
+    ...generateMetaDefault({
+        parameters: {
+            ...commonEmailsParameters,
+            docs: {
+                description: {
+                    component:
+                        "Email envoyé tous les 3 mois avec la liste des membres de la startus",
+                },
+            },
+        },
+    }),
+} as Meta<ComponentType>;
+
+const Template: StoryFn<ComponentType> = (args) => {
+    return <StartupNewMemberArrivalEmail {...args} />;
+};
+
+const NormalStory = Template.bind({});
+NormalStory.args = {
+    startup: {
+        uuid: "",
+        ghid: "agora",
+        name: "Agora",
+        incubator_id: "",
+        start: undefined,
+    },
+    userInfos: {
+        uuid: "",
+        username: "lucas.thenet",
+        fullname: "Lucas Thenet",
+        role: "Développeur Fullstack    ",
+        missions: [
+            {
+                start: new Date(),
+            },
+        ],
+        domaine: Domaine.ANIMATION,
+        primary_email_status: EmailStatusCode.EMAIL_ACTIVE,
+    },
+};
+NormalStory.decorators = [withEmailRenderer];
+NormalStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const Normal = prepareStory(NormalStory);
+
+const ClientOverviewStory = Template.bind({});
+ClientOverviewStory.args = {
+    ...NormalStory.args,
+};
+ClientOverviewStory.decorators = [
+    withEmailRenderer,
+    withEmailClientOverviewFactory(StartupNewMemberArrivalEmailTitle()),
+];
+ClientOverviewStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+};
+
+export const ClientOverview = prepareStory(ClientOverviewStory);
