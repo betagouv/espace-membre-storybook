782357062c5f5d8b3cd878d0d99dbc136c90f0bb
Lucas Charrier <lucharrier@gmail.com>
2025-04-30 09:12:36 +0200

    feat: add NoMoreContractXDaysEmail stories

---
+++ stories/emails/NoMoreContractXDaysEmail.stories.tsx
@@ -0,0 +1,73 @@
+import {
+    NoMoreContractXDaysEmail,
+    NoMoreContractXDaysEmailTitle,
+  } from "@/server/views/templates/emails/NoMoreContractEmail/NoMoreContractXDaysEmail";
+  import { StoryHelperFactory } from "@docs/.storybook/helpers";
+  import { playFindEmailStructure } from "@docs/.storybook/testing";
+  import {
+    commonEmailsParameters,
+    withEmailClientOverviewFactory,
+    withEmailRenderer,
+  } from "@docs/.storybook/email";
+  import { Meta, StoryFn } from "@storybook/react";
+  import { subDays } from "date-fns";
+  
+  type ComponentType = typeof NoMoreContractXDaysEmail;
+  const { generateMetaDefault, prepareStory } = StoryHelperFactory<ComponentType>();
+  
+  export default {
+    title: "Emails/Templates/NoMoreContractXDaysEmail",
+    component: NoMoreContractXDaysEmail,
+    ...generateMetaDefault({
+      parameters: {
+        ...commonEmailsParameters,
+        docs: {
+          description: {
+            component:
+              "Email envoyé à un membre dont la date de fin de mission est passée depuis 1 jour.",
+          },
+        },
+      },
+    }),
+  } as Meta<ComponentType>;
+  
+  const Template: StoryFn<ComponentType> = (args) => {
+    return <NoMoreContractXDaysEmail {...args} />;
+  };
+  
+  const NormalStory = Template.bind({});
+  NormalStory.args = {
+    user: {
+      fullname: "Madeleine Durand",
+      missions: [
+        {
+          id: "mission-123",
+          end: subDays(new Date(), 1), // hier
+        },
+        {
+          id: "mission-older",
+          end: subDays(new Date(), 30),
+        },
+      ],
+    },
+  };
+  NormalStory.decorators = [withEmailRenderer];
+  NormalStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+  };
+  
+  export const Normal = prepareStory(NormalStory);
+  
+  const ClientOverviewStory = Template.bind({});
+  ClientOverviewStory.args = {
+    ...NormalStory.args,
+  };
+  ClientOverviewStory.decorators = [
+    withEmailRenderer,
+    withEmailClientOverviewFactory(NoMoreContractXDaysEmailTitle()),
+  ];
+  ClientOverviewStory.play = async ({ canvasElement }) => {
+    await playFindEmailStructure(canvasElement);
+  };
+  
+  export const ClientOverview = prepareStory(ClientOverviewStory);
